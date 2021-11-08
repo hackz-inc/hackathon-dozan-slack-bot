7c38b51bc3f456630a9119f7f95c2cdd0cfeb31b
keisuke071411 <i.love.football0714@gmail.com>
2021-11-08 20:19:36 +0900

    Fix: default port number

--- main.go
+++ main.go
@@ -225,6 +225,9 @@ func main() {
 
 	log.Println("[INFO] Server listening")
 	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
 	if err := http.ListenAndServe(":" + port, nil); err != nil {
 		log.Fatal(err)
